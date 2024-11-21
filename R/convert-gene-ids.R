84c8b54d504a15d9d12a3d26e37a0222ea220adc
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2024-11-20 20:30:14 -0500

    Update gene id conversion and add docs

--- R/convert-gene-ids.R
+++ R/convert-gene-ids.R
@@ -1,27 +1,53 @@
-#' Title
+#' Convert Ensembl gene ids to gene symbols based on an ScPCA SingleCellExperiment object
 #'
-#' @param ensembl_ids A character vector of Ensembl gene ids to translate to gene symbols.
-#' @param sce A SingleCellExperiment object containing gene ids and gene symbols to use for translation.
+#' The SingleCellExperiment objects produced as part of ScPCA are indexed by
+#' Ensembl gene ids, as those are more stable than gene symbols. However,
+#' for many applications gene symbols are useful. This function provides a
+#' simple and consistent conversion of Ensembl gene ids to gene symbols based on
+#' the `gene_symbol` column that is present in the row data of ScPCA
+#' SingleCellExperiment objects.
+#'
+#' For this function, the SingleCellExperiment object must contain a `gene_ids`
+#' column containing Ensembl gene ids and a `gene_symbol` column containing gene
+#' symbols. If any gene ids are not found or if the gene symbol is not defined,
+#' the input gene id is returned, unless the `leave_na` is set to `TRUE`.
+#'
+#'
+#' @param ensembl_ids A character vector of Ensembl gene ids to translate to
+#'  gene symbols.
+#' @param sce A SingleCellExperiment object containing gene ids and gene symbols
+#'  to use for translation.
+#' @param leave_na logical indicating whether to leave NA values in the output.
+#'  Default is `FALSE`
 #'
 #' @return A vector of gene symbols corresponding to the input Ensembl ids.
 #' @export
 #'
-#' @imports SingleCellExperiment
+#' @import SingleCellExperiment
 #'
 #' @examples
-ensembl_to_symbol <- function(ensembl_ids, sce) {
+#' \dontrun{
+#' # convert a set of Ensembl ids to gene symbols
+#' # using a SingleCellExperiment reference
+#' ensembl_ids <- c("ENSG00000141510", "ENSG00000134323")
+#' gene_symbols <- ensembl_to_symbol(ensembl_ids, sce)
+#' gene_symbols
+#' ### [1] "TP53" "MYCN"
+#' }
+ensembl_to_symbol <- function(ensembl_ids, sce, leave_na = FALSE) {
   stopifnot(
     "`sce` must be a SingleCellExperiment object ." = is(sce, "SingleCellExperiment"),
     "`ensembl_ids` must be a character vector." = is.character(ensembl_ids),
     "`sce` must contain both a `gene_ids` and `gene_symbol` column in the row data." =
       all(c("gene_ids", "gene_symbol") %in% names(rowData(sce))),
+    "`leave_na` must be TRUE or FALSE." = is.logical(leave_na)
   )
 
   id_match <- match(ensembl_ids, rowData(sce)$gene_ids)
   gene_symbols <- rowData(sce)[id_match, "gene_symbol"]
 
   missing_symbols <- is.na(gene_symbols)
-  if (any(missing_symbols)) {
+  if (!leave_na && any(missing_symbols)) {
     warning("Not all `ensembl_ids` are present in sce `gene_ids`, using input ids for missing values.")
     gene_symbols[missing_symbols] <- ensembl_ids[missing_symbols]
   }
@@ -29,7 +55,19 @@ ensembl_to_symbol <- function(ensembl_ids, sce) {
   return(gene_symbols)
 }
 
-#' Title
+#' Set the row names of an ScPCA SingleCellExperiment object to gene symbols
+#'
+#' The SingleCellExperiment objects produced as part of ScPCA are indexed by
+#' Ensembl gene ids, as those are more stable than gene symbols. However,
+#' for many applications gene symbols are useful. This function converts the
+#' row names (indexes) of a SingleCellExperiment object to gene symbols based.
+#'
+#' Internal data structures such as the list of highly variable genes and the
+#' rotation matrix for the PCA are also updated to use gene symbols, if present
+#' (and not disabled by the `convert_hvg` and `convert_pca` arguments).
+#'
+#' Note that using this function will result in non-unique row ids as no
+#' de-duplication is currently performed.
 #'
 #' @param sce A SingleCellExperiment object containing gene ids and gene symbols.
 #' @param convert_hvg Logical indicating whether to convert highly variable genes to gene symbols.
@@ -38,9 +76,13 @@ ensembl_to_symbol <- function(ensembl_ids, sce) {
 #' @return A SingleCellExperiment object with row names set as gene symbols.
 #' @export
 #'
-#' @imports SingleCellExperiment
+#' @import SingleCellExperiment
 #'
 #' @examples
+#' \dontrun{
+#' # convert a SingleCellExperiment object to use gene symbols
+#' symbol_sce <- sce_to_symbols(sce)
+#' }
 sce_to_symbols <- function(sce, convert_hvg = TRUE, convert_pca = TRUE) {
   stopifnot(
     "`sce` must be a SingleCellExperiment object." = is(sce, "SingleCellExperiment"),
