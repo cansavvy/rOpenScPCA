3e8fda91a7bca176fff05120660279e85cb0e270
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2024-11-26 11:53:50 -0500

    Update gene symbol conversion

--- R/convert-gene-ids.R
+++ R/convert-gene-ids.R
@@ -1,24 +1,25 @@
-#' Convert Ensembl gene ids to gene symbols based on an ScPCA SingleCellExperiment object
+#' Convert Ensembl gene ids to gene symbols based on reference gene lists
 #'
 #' The SingleCellExperiment objects produced as part of ScPCA are indexed by
 #' Ensembl gene ids, as those are more stable than gene symbols. However,
-#' for many applications gene symbols are useful. This function provides a
-#' simple and consistent conversion of Ensembl gene ids to gene symbols based on
-#' the `gene_symbol` column that is present in the row data of ScPCA
-#' SingleCellExperiment objects.
+#' for many applications gene symbols are useful. This function provides
+#' simple conversion of Ensembl gene ids to gene symbols based on either the
+#' ScPCA reference gene list or a 10x reference gene list as used by Cell Ranger.
 #'
-#' For this function, the SingleCellExperiment object must contain a `gene_ids`
-#' column containing Ensembl gene ids and a `gene_symbol` column containing gene
-#' symbols. If any gene ids are not found or if the gene symbol is not defined,
-#' the input gene id is returned, unless the `leave_na` is set to `TRUE`.
+#' The gene symbols can either be made unique (as would be done if read in by Seurat)
+#' or left as is.
 #'
 #'
 #' @param ensembl_ids A character vector of Ensembl gene ids to translate to
 #'  gene symbols.
-#' @param sce A SingleCellExperiment object containing gene ids and gene symbols
-#'  to use for translation.
-#' @param leave_na logical indicating whether to leave NA values in the output.
-#'  Default is `FALSE`
+#' @param reference The reference gene list to use for translation. One of `scpca`,
+#'   `10x2020`, `10x2024`. The `scpca` reference is the default.
+#' @param sce A SingleCellExperiment object to use as a reference for gene symbols.
+#'  If provided, the `reference` argument will be ignored. The `sce` object must
+#'  include columns with the names `gene_ids` and `gene_symbol` to use for conversion.
+#' @param unique Whether to use unique gene symbols, as would be done if
+#'  data had been read in with gene symbols by Seurat. Default is FALSE.
+#' @param leave_na Whether to leave NA values in the output vector. Default is FALSE.
 #'
 #' @return A vector of gene symbols corresponding to the input Ensembl ids.
 #' @export
@@ -26,25 +27,40 @@
 #' @import SingleCellExperiment
 #'
 #' @examples
-#' \dontrun{
 #' # convert a set of Ensembl ids to gene symbols
-#' # using a SingleCellExperiment reference
 #' ensembl_ids <- c("ENSG00000141510", "ENSG00000134323")
-#' gene_symbols <- ensembl_to_symbol(ensembl_ids, sce)
+#' gene_symbols <- ensembl_to_symbol(ensembl_ids)
 #' gene_symbols
 #' ### [1] "TP53" "MYCN"
-#' }
-ensembl_to_symbol <- function(ensembl_ids, sce, leave_na = FALSE) {
+#'
+ensembl_to_symbol <- function(
+    ensembl_ids,
+    reference = c("scpca", "10x2020", "10x2024"),
+    sce = NULL,
+    unique = FALSE,
+    leave_na = FALSE) {
+  reference <- match.arg(reference)
   stopifnot(
-    "`sce` must be a SingleCellExperiment object ." = is(sce, "SingleCellExperiment"),
     "`ensembl_ids` must be a character vector." = is.character(ensembl_ids),
+    "`sce` must be a SingleCellExperiment object or NULL." = is.null(sce) || inherits(sce, "SingleCellExperiment"),
     "`sce` must contain both a `gene_ids` and `gene_symbol` column in the row data." =
-      all(c("gene_ids", "gene_symbol") %in% names(rowData(sce))),
-    "`leave_na` must be TRUE or FALSE." = is.logical(leave_na)
+      is.null(sce) || all(c("gene_ids", "gene_symbol") %in% names(rowData(sce))),
+    "`unique` must be TRUE or FALSE." = is.logical(unique) && !is.na(unique),
+    "`leave_na` must be TRUE or FALSE." = is.logical(leave_na) && !is.na(leave_na)
   )
 
-  id_match <- match(ensembl_ids, rowData(sce)$gene_ids)
-  gene_symbols <- rowData(sce)[id_match, "gene_symbol"]
+  if (is.null(sce)) {
+    # build the symbol column name
+    symbol_column <- paste0("gene_symbol_", reference, ifelse(unique, "_unique", ""))
+    # get the gene symbols
+    gene_symbols <- scpca_gene_reference[match(ensembl_ids, scpca_gene_reference$gene_ids), symbol_column]
+  } else {
+    all_symbols <- rowData(sce)$gene_symbol
+    if (unique) {
+      all_symbols <- make.unique(all_symbols)
+    }
+    gene_symbols <- all_symbols[match(ensembl_ids, rowData(sce)$gene_ids)]
+  }
 
   missing_symbols <- is.na(gene_symbols)
   if (!leave_na && any(missing_symbols)) {
--- tests/testthat/test-convert-gene-ids.R
+++ tests/testthat/test-convert-gene-ids.R
@@ -1,24 +1,59 @@
-# read in SCE for testing
-sce <- readRDS(test_path("data", "scpca_sce.rds"))
-
-test_that("basic ensembl_id conversion works", {
+test_that("basic gene symbol conversion works", {
   ensembl_ids <- c("ENSG00000141510", "ENSG00000134323")
-  gene_symbols <- ensembl_to_symbol(ensembl_ids, sce)
+  gene_symbols <- ensembl_to_symbol(ensembl_ids)
 
   expect_equal(gene_symbols, c("TP53", "MYCN"))
 })
 
-test_that("ensembl_id conversion works with unexpected ids", {
+test_that("gene symbol conversion works with unexpected ids", {
   ensembl_ids <- c("ENSG00000141510", "ENSG00000134323", "foobar")
 
-  expect_warning(gene_symbols <- ensembl_to_symbol(ensembl_ids, sce))
+  expect_warning(gene_symbols <- ensembl_to_symbol(ensembl_ids))
   expect_equal(gene_symbols, c("TP53", "MYCN", "foobar"))
 
-  expect_no_warning(gene_symbols_na <- ensembl_to_symbol(ensembl_ids, sce, leave_na = TRUE))
+  expect_no_warning(gene_symbols_na <- ensembl_to_symbol(ensembl_ids, leave_na = TRUE))
   expect_equal(gene_symbols_na, c("TP53", "MYCN", NA))
 })
 
+test_that("gene symbol conversion works for 10x references", {
+  ensembl_ids <- c("ENSG00000141510", "ENSG00000134323")
+  gene_symbols <- ensembl_to_symbol(ensembl_ids, reference = "10x2020")
+
+  expect_equal(gene_symbols, c("TP53", "MYCN"))
+
+  gene_symbols <- ensembl_to_symbol(ensembl_ids, reference = "10x2024")
+
+  expect_equal(gene_symbols, c("TP53", "MYCN"))
+})
+
+test_that("gene symbol conversion works for 'unique' gene symbols", {
+  ensembl_ids <- c("ENSG00000015479", "ENSG00000269226")
+  gene_symbols <- ensembl_to_symbol(ensembl_ids, reference = "scpca", unique = FALSE)
+  expect_equal(gene_symbols, c("MATR3", "TMSB15B"))
+  gene_symbols <- ensembl_to_symbol(ensembl_ids, reference = "scpca", unique = TRUE)
+  expect_equal(gene_symbols, c("MATR3.1", "TMSB15B.1"))
+
+  gene_symbols <- ensembl_to_symbol(ensembl_ids, reference = "10x2020", unique = FALSE)
+  expect_equal(gene_symbols, c("MATR3", "TMSB15B"))
+  gene_symbols <- ensembl_to_symbol(ensembl_ids, reference = "10x2020", unique = TRUE)
+  expect_equal(gene_symbols, c("MATR3.1", "TMSB15B.1"))
+})
+
+test_that("gene symbol conversion works using an SCE reference", {
+  sce <- readRDS(test_path("data", "scpca_sce.rds"))
+  ensembl_ids <- c("ENSG00000015479", "ENSG00000269226")
+  gene_symbols <- ensembl_to_symbol(ensembl_ids, sce = sce, unique = FALSE)
+  expect_equal(gene_symbols, c("MATR3", "TMSB15B"))
+
+  gene_symbols <- ensembl_to_symbol(ensembl_ids, sce = sce, unique = TRUE)
+  expect_equal(gene_symbols, c("MATR3.1", "TMSB15B.1"))
+})
+
+
+
 test_that("conversion of a full sce object works as expected", {
+  sce <- readRDS(test_path("data", "scpca_sce.rds"))
+
   expect_warning(converted_sce <- sce_to_symbols(sce))
 
   gene_symbols <- rowData(sce)$gene_symbol
