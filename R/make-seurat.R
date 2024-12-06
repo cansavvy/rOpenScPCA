6a316f72645019b4769bd0abd0a79491bbdefd21
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2024-12-06 15:38:56 -0500

    Add make-seurat functions and tests

---
+++ R/make-seurat.R
@@ -0,0 +1,122 @@
+#' Convert an SCE object to Seurat
+#'
+#' Converts A SingleCellExperiment (SCE) object to Seurat format. This is
+#' primarily a wrapper around Seurat::as.Seurat() with some additional steps to
+#' include ScPCA metadata and options for converting the feature index from
+#' Ensembl gene ids to gene symbols.
+#'
+#' If present, reduced dimensions from SCE objects will be included, renamed to
+#' match Seurat default naming.
+#'
+#'
+#' @param sce The input SCE object
+#' @param use_symbols Logical indicated whether the Seurat object use gene
+#'   symbols for indexing. Default is TRUE.
+#' @param reference If `use_symbols` is TRUE, the reference to use for gene
+#'   symbols. One of `sce`, `scpca`, `10x2020`, `10x2024`, where `sce` uses the
+#'   symbols stored in the `gene_symbol` column of the SCE object's row data,
+#'   and the others use standardized translation tables. See `ensembl_to_symbol`
+#'   for more information. Default is `sce`.
+#' @param dedup_method Method to handle duplicated gene symbols. If `unique`,
+#'   the gene symbols will be made unique following standard Seurat procedures.
+#'   If `sum`, the expression values for each gene symbols will be summed.
+#' @param seurat_assay_version Whether to create Seurat `v3` or `v5` assay
+#'   objects. Default is `v3`.
+#'
+#' @return a Seurat object
+#' @export
+#'
+sce_to_seurat <- function(
+    sce,
+    use_symbols = TRUE,
+    reference = c("sce", "scpca", "10x2020", "10x2024"),
+    dedup_method = c("unique", "sum"),
+    seurat_assay_version = c("v3", "v5")) {
+  reference <- match.arg(reference)
+  dedup_method <- match.arg(dedup_method)
+  seurat_assay_version <- match.arg(seurat_assay_version)
+
+  stopifnot(
+    "Package `Seurat` must be installed for SCE to Seurat conversion." =
+      requireNamespace("Seurat", quietly = TRUE),
+    "Package `SeuratObject` must be installed for SCE to Seurat conversion." =
+      requireNamespace("SeuratObject", quietly = TRUE),
+    "`sce` must be a SingleCellExperiment object." = is(sce, "SingleCellExperiment"),
+    "`sce` must contain both `gene_ids` and `gene_symbol` columns in the row data if it is being used as a reference." =
+      reference != "sce" || all(c("gene_ids", "gene_symbol") %in% names(rowData(sce)))
+  )
+
+  if (use_symbols) {
+    sce <- suppressMessages(sce_to_symbols(
+      sce,
+      reference = reference,
+      unique = (dedup_method == "unique"),
+      seurat_compatible = TRUE
+    ))
+  }
+
+  if (dedup_method == "sum") {
+    sce <- sum_duplicate_genes(sce)
+  }
+
+  mainExpName(sce) <- "RNA"
+
+  # convert reducedDims to Seurat standard naming
+  reducedDimNames(sce) <- tolower(reducedDimNames(sce))
+  reducedDims(sce) <- reducedDims(sce) |>
+    purrr::map(\(mat) {
+      # insert underscore between letters and numbers in axis labels
+      names <- gsub("([A-Za-z]+)([0-9]+)", "\\1_\\2", colnames(mat)) |>
+        tolower()
+      colnames(mat) <- names
+      if (!is.null(attr(mat, "rotation"))) {
+        colnames(attr(mat, "rotation")) <- names
+      }
+      return(mat)
+    })
+
+  # Let Seurat do initial conversion to capture most things
+  sobj <- Seurat::as.Seurat(sce)
+
+  # update identity values, using cluster if present
+  sce_meta <- metadata(sce)
+  sobj$orig.ident <- sce_meta$library_id
+  if (is.null(sce$cluster)) {
+    Seurat::Idents(sobj) <- sce_meta$library_id
+  } else {
+    Seurat::Idents(sobj) <- sce$cluster
+  }
+
+  # add variable features if present
+  if (!is.null(sce_meta$highly_variable_genes)) {
+    sobj[["RNA"]]@var.features <- sce_meta$highly_variable_genes
+    sce_meta$highly_variable_genes <- NULL # remove from metadata to avoid redundancy
+  }
+
+  # move rotation to feature.loadings
+
+
+  # convert and set functions depending on assay version requested
+  if (seurat_assay_version == "v5") {
+    create_seurat_assay <- SeuratObject::CreateAssay5Object
+    sobj[["RNA"]] <- as(sobj[["RNA"]], "Assay5")
+  } else {
+    create_seurat_assay <- SeuratObject::CreateAssayObject
+    sobj[["RNA"]] <- as(sobj[["RNA"]], "Assay")
+  }
+
+  # add spliced data if present
+  if ("spliced" %in% assayNames(sce)) {
+    sobj[["spliced"]] <- create_seurat_assay(counts = assay(sce, "spliced"))
+  }
+
+  # add sample-level metadata after removing non-vector objects
+  sobj@misc <- sce_meta |>
+    purrr::discard(is.object) |>
+    purrr::discard(is.list)
+
+  # scale data because many Seurat functions expect this
+  sobj <- Seurat::ScaleData(sobj, assay = "RNA", verbose = FALSE)
+
+  return(sobj)
+}
---
+++ tests/testthat/test-make-seurat.R
@@ -0,0 +1,122 @@
+test_that("SCE to Seurat with no id conversion works", {
+  sce <- readRDS(test_path("data", "scpca_sce.rds"))
+  seurat_obj <- sce_to_seurat(sce, use_symbols = FALSE)
+
+  expect_equal(dim(seurat_obj), dim(sce))
+  expect_setequal(rownames(seurat_obj), rownames(sce))
+  expect_setequal(colnames(seurat_obj), colnames(sce))
+
+  expect_equal(names(seurat_obj@assays), c("RNA", "spliced"))
+  expect_equal(Seurat::DefaultAssay(seurat_obj), "RNA")
+
+  # array contents
+  expect_contains(
+    slotNames(seurat_obj[["RNA"]]),
+    c("counts", "data", "scale.data", "var.features", "meta.features")
+  )
+
+  expect_equal(seurat_obj[["RNA"]]$counts, counts(sce))
+  expect_equal(seurat_obj[["RNA"]]$data, logcounts(sce))
+
+  expect_setequal(
+    seurat_obj[["RNA"]]@var.features,
+    metadata(sce)$highly_variable_genes
+  )
+
+  expect_equal(names(seurat_obj@reductions), c("pca", "umap"))
+  expect_equal(
+    dim(seurat_obj[["pca"]]),
+    dim(reducedDim(sce, "PCA"))
+  )
+  expect_equal(
+    dim(seurat_obj[["umap"]]),
+    dim(reducedDim(sce, "UMAP"))
+  )
+})
+
+
+test_that("SCE to Seurat with id conversion works as expected", {
+  sce <- readRDS(test_path("data", "scpca_sce.rds"))
+  seurat_obj <- sce_to_seurat(sce, use_symbols = TRUE, reference = "sce")
+
+  new_genes <- suppressMessages(
+    ensembl_to_symbol(rownames(sce), unique = TRUE, sce = sce)
+  )
+  new_genes <- gsub("_", "-", new_genes)
+
+  hv_genes <- suppressMessages(
+    ensembl_to_symbol(metadata(sce)$highly_variable_genes, unique = TRUE, sce = sce)
+  )
+  hv_genes <- gsub("_", "-", hv_genes)
+
+  expect_equal(dim(seurat_obj), dim(sce))
+  expect_setequal(colnames(seurat_obj), colnames(sce))
+  expect_setequal(rownames(seurat_obj), new_genes)
+
+  expect_equal(names(seurat_obj@assays), c("RNA", "spliced"))
+  expect_equal(Seurat::DefaultAssay(seurat_obj), "RNA")
+
+  # array contents
+  expect_contains(
+    slotNames(seurat_obj[["RNA"]]),
+    c("counts", "data", "scale.data", "var.features", "meta.features")
+  )
+
+  expect_setequal(
+    seurat_obj[["RNA"]]@var.features,
+    hv_genes
+  )
+  expect_equal(names(seurat_obj@reductions), c("pca", "umap"))
+  expect_equal(names(seurat_obj@reductions), c("pca", "umap"))
+  expect_equal(
+    dim(seurat_obj[["pca"]]),
+    dim(reducedDim(sce, "PCA"))
+  )
+  expect_equal(
+    dim(seurat_obj[["umap"]]),
+    dim(reducedDim(sce, "UMAP"))
+  )
+})
+
+test_that("SCE to Seurat with id conversion and 10x reference works as expected", {
+  sce <- readRDS(test_path("data", "scpca_sce.rds"))
+  seurat_obj <- sce_to_seurat(sce, use_symbols = TRUE, reference = "10x2020")
+
+  new_genes <- suppressMessages(
+    ensembl_to_symbol(rownames(sce), unique = TRUE, reference = "10x2020")
+  )
+  new_genes <- gsub("_", "-", new_genes)
+
+  hv_genes <- suppressMessages(
+    ensembl_to_symbol(metadata(sce)$highly_variable_genes, unique = TRUE, reference = "10x2020")
+  )
+  hv_genes <- gsub("_", "-", hv_genes)
+
+  expect_equal(dim(seurat_obj), dim(sce))
+  expect_setequal(colnames(seurat_obj), colnames(sce))
+  expect_setequal(rownames(seurat_obj), new_genes)
+
+  expect_equal(names(seurat_obj@assays), c("RNA", "spliced"))
+  expect_equal(Seurat::DefaultAssay(seurat_obj), "RNA")
+
+  # array contents
+  expect_contains(
+    slotNames(seurat_obj[["RNA"]]),
+    c("counts", "data", "scale.data", "var.features", "meta.features")
+  )
+
+  expect_setequal(
+    seurat_obj[["RNA"]]@var.features,
+    hv_genes
+  )
+  expect_equal(names(seurat_obj@reductions), c("pca", "umap"))
+  expect_equal(names(seurat_obj@reductions), c("pca", "umap"))
+  expect_equal(
+    dim(seurat_obj[["pca"]]),
+    dim(reducedDim(sce, "PCA"))
+  )
+  expect_equal(
+    dim(seurat_obj[["umap"]]),
+    dim(reducedDim(sce, "UMAP"))
+  )
+})
